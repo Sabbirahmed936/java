d3579b8119496bdc62a50eee31c35c14082ec563
Sabbirahmed936 <46643357+Sabbirahmed936@users.noreply.github.com>
2019-04-16 00:37:02 -0700

    New

---
+++ Main.java
@@ -0,0 +1,38 @@
+package labExam2;
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import labExam2.Academic;
+import labExam2.PerDetails;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Socket s=new Socket("localhost",3333);  
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+		  
+		String str="",str2="";  
+		while(!str.equals("stop")){  
+		str=br.readLine();  
+		dout.writeUTF(str);  
+		dout.flush();  
+		str2=din.readUTF();  
+		System.out.println("Server says: "+str2);  
+		}  
+		  
+		dout.close();  
+		s.close();  
+		}
+	}
+	
+
+
